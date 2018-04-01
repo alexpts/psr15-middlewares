9e430a62a75dfa26c95b57b8d7a34ec0ae343822
alexpts <alexpts@yandex.ru>
2018-04-01 10:41:38 +0300

    add middleware `StaticResponse`

--- README.md
+++ README.md
@@ -38,3 +38,5 @@ Middlewares compatible with [PSR-7](https://www.php-fig.org/psr/psr-7/) and [PSR
 ### JsonPayload (need tests)
 
 ### RequestWithAttribute
+
+### StaticResponse (need tests)
---
+++ src/PTS/PSR15/Middlewares/StaticResponse.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace PTS\PSR15\Middlewares;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class StaticResponse implements MiddlewareInterface
+{
+    /** @var ResponseInterface */
+    protected $response;
+
+    public function __construct(ResponseInterface $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * @param ServerRequestInterface  $request
+     * @param RequestHandlerInterface $next
+     *
+     * @return ResponseInterface
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $next) : ResponseInterface
+    {
+        return clone $this->response;
+    }
+}
